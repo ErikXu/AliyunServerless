3eff59d98821a73b4c1373e4115747004d8b2271
Erik Xu <81636759@qq.com>
2019-10-13 22:36:16 +0800

    Init.

---
+++ AliyunServerless/AliyunServerless/IpHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Net;
+using Aliyun.Serverless.Core;
+using Microsoft.Extensions.Logging;
+
+namespace AliyunServerless
+{
+    /// <summary>
+    /// 打印服务器IP
+    /// </summary>
+    public class IpHandler
+    {
+        public void Handle(Stream input, IFcContext context)
+        {
+            ILogger logger = context.Logger;
+            var ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            logger.LogInformation($"Handle request: {context.RequestId}");
+
+            foreach (var ipAddress in ipHostInfo.AddressList)
+            {
+                logger.LogInformation($"IP Address：{ipAddress}");
+            }
+        }
+    }
+}
---
+++ AliyunServerless/AliyunServerless/LogHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Aliyun.Serverless.Core;
+using Microsoft.Extensions.Logging;
+
+namespace AliyunServerless
+{
+    /// <summary>
+    /// 打印执行日志
+    /// </summary>
+    public class LogHandler
+    {
+        public void Handle(Stream input, IFcContext context)
+        {
+            ILogger logger = context.Logger;
+            logger.LogInformation($"Handle request: {context.RequestId}");
+        }
+    }
+}
